f75ff02c43f8c4b3fc9283b37ed938fb2f16ed47
David Nagy <david.nagy@quinto.io>
2023-04-13 19:21:38 +0200

    0.30.2

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,22 @@
-export default  {
+export default {
   testRegex: "(/tests/*.test.ts|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   coverageReporters: ["html", "text", "text-summary", "cobertura", "lcov"],
   collectCoverageFrom: ["**/*.ts", "!**/node_modules/**","!tests/**"],
   testPathIgnorePatterns: ["/dist/", "/node_modules/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+  extensionsToTreatAsEsm: [".ts"],
+  setupFilesAfterEnv: ["./node_modules/@grandlinex/core/jest.pre.config.js"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
   },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        "useESM": true
+      }
+    ]
+  }
 };
