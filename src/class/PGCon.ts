1bb337bd2ea9060dfa4fd5eb6ce12f40cb099e9d
David Nagy <david.nagy@quinto.io>
2025-08-21 11:50:51 +0200

    1.1.1

--- src/class/PGCon.ts
+++ src/class/PGCon.ts
@@ -232,8 +232,6 @@ export default class PGCon<
     const param: any[] = [];
     if (search) {
       searchQ = buildSearchQ<E>(config, search, param, searchQ);
-      this.debug(searchQ);
-      this.debug(param);
     }
     if (order && order.length > 0) {
       order.forEach((val) => {
